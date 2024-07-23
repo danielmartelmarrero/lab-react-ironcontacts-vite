6292ee87a0e84c0322b220f433ff5f3d0758fca0
Daniel Martel <danielmartelmarrero@gmail.com>
2024-07-23 15:07:54 +0100

    Tryed my best

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,50 @@
+import { useState } from "react";
 import "./App.css";
+import contactsList from "./contacts.json"
+
 
 function App() {
+  const [contacts, setContacts] = useState(contactsList.slice(0,5))
+  
+  
+  function getRandomContact(){
+    let randomIndex = Math.floor(Math.random()* contactsList.length);
+    let randomContact = contacts[randomIndex]
+
+    return randomContact
+  }
+
+  const newContact = () => {
+    const newContact = getRandomContact();
+    setSelectedContacts([...contacts, newContact])
+  }
+
   return (
     <div className="App">
-      <h1>LAB | React IronContacts</h1>
+      <h1>IronContacts</h1>
+      <button disabled={contacts.length == contactsList.length} onClick={newContact}>Add New Contact</button>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emmy</th>
+        </tr>
+        
+        {contacts.map((oneContact)=>{
+          return (<tr key={oneContact.id}>
+            <td><img src={oneContact.pictureUrl} alt="" /></td>
+            <td>{oneContact.name}</td>
+            <td>{(oneContact.popularity).toFixed(2)}</td>
+            <td>{oneContact.wonOscar ? "🏆" : ""}</td>
+            <td>{oneContact.wonEmmy ? "🌟" : ""}</td>
+          </tr>)
+        
+
+        })}
+        
+      </table>
     </div>
   );
 }
